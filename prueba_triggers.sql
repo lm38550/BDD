4e9491c0998b5f8990ad791437d78e2bef3d74fe
Frederik Maenner <frederikmaenner@cvi122156.ugr.es>
2023-12-11 19:05:28 +0100

    updated values

--- prueba_triggers.sql
+++ prueba_triggers.sql
@@ -1,12 +1,12 @@
 INSERT INTO Productores VALUES (1, 'a1', 'pablo', 'calle molinos 1');
 
-INSERT INTO Sucursales VALUES (1, "antonio", "granada", NULL, "Andalucía")
+INSERT INTO Sucursales VALUES (1, 'antonio', 'granada', NULL, 'Andalucía')
 
-INSERT INTO Clientes VALUES (1, "a2", "b", "fred", "calle malaga", "Andalucía")
+INSERT INTO Clientes VALUES (1, 'a2', 'b', 'fred', 'calle malaga', 'Andalucía')
 
-INSERT INTO Empleados VALUES (1, "a3", "paul", "calle granada", TODATE(20230901), 2000, 1, 1)
+INSERT INTO Empleados VALUES (1, 'a3', 'paul', 'calle granada', TODATE(20230901), 2000, 1, 1)
 
-INSERT INTO Vinos VALUES (1, rioja, "Andalucía", 2008, NULL, "hola", "adios", 10, 10, 1, 1)
+INSERT INTO Vinos VALUES (1, rioja, 'Andalucía', 2008, NULL, 'hola', 'adios', 10, 10, 1, 1)
 
 INSERT INTO Pides VALUES (TODATE(20231001), 2, 1, 1, 1)
 
