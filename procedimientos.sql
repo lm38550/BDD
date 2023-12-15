66892feabc385b719960e508ea48652bacf04947
Frederik Maenner <frederikmaenner@Frederiks-MacBook-Air.local>
2023-12-15 11:04:11 +0100

    procedimientos with constraints

--- procedimientos.sql
+++ procedimientos.sql
@@ -125,3 +125,162 @@ EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
 END nuevaSucursal;
+
+
+CREATE OR REPLACE PROCEDURE cambiarDirector(
+    p_codigo_sucursal NUMBER,
+    p_codigo_director VARCHAR2
+) IS
+BEGIN
+    UPDATE Sucursales
+    SET
+        director = p_codigo_director
+    WHERE
+        codigo = p_codigo_sucursal;
+    
+    DBMS_OUTPUT.PUT_LINE('Director insertado o actualizado');
+    
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END cambiarDirector;
+
+
+CREATE OR REPLACE PROCEDURE nuevoCliente(
+    p_codigo NUMBER,
+    p_DNI VARCHAR2,
+    p_tipo VARCHAR2,
+    p_nombre VARCHAR2,
+    p_direccion VARCHAR2,
+    p_comunidadAutonoma VARCHAR2
+) IS
+BEGIN
+    INSERT INTO Clientes
+        codigo,
+        DNI,
+        tipo,
+        nombre,
+        direccion,
+        comunidadAutonoma
+    VALUES
+        p_codigo_director,
+        p_DNI,
+        p.tipo,
+        p.nombre,
+        p.direccion,
+        p.comunidadAutonoma
+    
+    DBMS_OUTPUT.PUT_LINE('Cliente creada');
+    
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END nuevoCliente;
+
+
+CREATE OR REPLACE PROCEDURE nuevoSuministro(
+    p_codigo_cliente NUMBER,
+    p_codigo_sucursal NUMBER,
+    p_codigo_vino NUMBER,
+    p_fecha_solicitud DATE,
+    p_cantidad NUMBER
+) AS
+    v_exists NUMBER;
+    v_comunidad_autonoma_cliente VARCHAR2,
+    v_comunidad_autonoma_sucursal VARCHAR2,
+    v_comunidad_autonoma_vino VARCHAR2,
+    v_codigo_sucursal_entregador NUMBER
+
+BEGIN
+
+    SELECT COUNT(*)
+    INTO v_exists
+    FROM Suministros
+    WHERE codigo_sucursal = p_codigo_sucursal
+    AND codigo_vino = p_codigo_vino
+    AND fecha = p_fecha_solicitud;
+
+    SELECT comunidadAutonoma -- get comunidad autonoma for first ELSE IF statement
+    INTO v_comunidad_autonoma_sucursal
+    FROM Sucursales
+    WHERE codigo = p_codigo_sucursal;
+
+    SELECT comunidadAutonoma -- get comunidad autonoma for first ELSE IF statement
+    INTO v_comunidad_autonoma_cliente
+    FROM Clientes
+    WHERE codigo = p_codigo_cliente;
+
+    SELECT comunidadAutonoma -- get comunidad autonoma for second IF statement
+    INTO v_comunidad_autonoma_vino
+    FROM Vinos
+    WHERE codigo = p.codigo_vino;
+
+
+    IF v_exists > 0 THEN -- suministro already exists and only the additional amount needs to be added
+        UPDATE Suministros
+        SET cantidad = cantidad + p_cantidad
+        WHERE codigo_sucursal = p_codigo_sucursal
+        AND codigo_vino = p_codigo_vino
+        AND fecha = p_fecha_solicitud;
+        DBMS_OUTPUT.PUT_LINE('Suministro creada');
+
+    ELSE IF v_comunidad_autonoma_cliente = v_comunidad_autonoma_sucursal -- customer is in the same CA as the sucursal
+
+        IF v_comunidad_autonoma_vino = v_comunidad_autonoma_sucursal -- check if the requested vine is distributed by the customer's sucursal
+            INSERT INTO Suministros(cantidad, fecha, codigo_vino, p_codigo_cliente, codigo_sucursal)
+            VALUES (p_cantidad, p_fecha_solicitud, p_codigo_vino, p_codigo_cliente, p_codigo_sucursal);
+            DBMS_OUTPUT.PUT_LINE('Suministro creada');
+        ELSE -- Wine is not distributed by the customer's sucursal
+            SELECT FIRST codigo -- get the codigo of a sucursal which can provide the requested vine. If there are more than one, the first will be chosen
+            INTO v_codigo_sucursal_entregador
+            FROM Sucursales
+            WHERE comunidadAutonoma = v_comunidad_autonoma_vino;
+
+            EXEC nuevoPedido(v_codigo_sucursal_entregador, p_codigo_sucursal, p_codigo_vino, p_fecha_solicitud, p_cantidad); -- Customer's sucursal orders wine from another sucursal which distributes it
+            INSERT INTO Suministros(cantidad, fecha, codigo_vino, p_codigo_cliente, codigo_sucursal)
+            VALUES (p_cantidad, p_fecha_solicitud, p_codigo_vino, p_codigo_cliente, p_codigo_sucursal);
+            DBMS_OUTPUT.PUT_LINE('Suministro creada');
+        END IF;
+
+    ELSE -- customer is not in the same CA as the sucursal
+        DBMS_OUTPUT.PUT_LINE('An order can only be placed with a Sucursal that is in your own Comunidad Autonoma');
+    END IF;
+    
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END nuevoSuministro;
+
+
+
+CREATE OR REPLACE PROCEDURE bajaSuministro(
+    p_codigo_cliente NUMBER,
+    p_codigo_sucursal NUMBER,
+    p_codigo_vino NUMBER,
+    p_fecha_Suministro DATE DEFAULT NULL
+) AS
+BEGIN
+    IF p_fecha_Suministro IS NULL THEN
+        DELETE FROM Suministros
+        WHERE codigo_sucursal = p_codigo_sucursal
+        AND codigo_vino = p_codigo_vino
+        AND p_codigo_cliente = p_codigo_cliente;
+    ELSE
+        DELETE FROM Suministros
+        WHERE codigo_sucursal = p_codigo_sucursal
+        AND codigo_vino = p_codigo_vino
+        AND fecha = p_fecha_Suministro
+        AND p_codigo_cliente = p_codigo_cliente;
+    END IF;
+    
+    DBMS_OUTPUT.PUT_LINE('Suministro eliminado');
+    
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
+END bajaSuministro;
+
