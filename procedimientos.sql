c6f8abd02be178c759bb5c7cef5e297fa40f93e9
lm38550 <lm38550@gmail.com>
2023-12-27 10:34:42 +0100

    Added unique code verifications

--- procedimientos.sql
+++ procedimientos.sql
@@ -7,32 +7,40 @@ CREATE OR REPLACE PROCEDURE nuevoEmpleado(
     p_salario NUMBER,
     p_codigo_sucursal NUMBER
 ) IS
+    v_codigo NUMBER(10);
 BEGIN
+    SELECT COUNT(*) INTO v_codigo
+    FROM Empleados
+    WHERE codigo = p_codigo;
     -- Insertar datos en la tabla de empleados
-    INSERT INTO Empleados (
-        codigo,
-        DNI,
-        nombre,
-        direccion,
-        fechaDeComienzo,
-        salario,
-        trabajaPorLaSucursal,
-        codigo_sucursal
-    ) VALUES (
-        p_codigo,
-        p_dni,
-        p_nombre,
-        p_direccion,
-        p_fechaDeComienzo,
-        p_salario,
-        1,
-        p_codigo_sucursal
-    );
 
-    DBMS_OUTPUT.PUT_LINE('Empleado creado exitosamente.');
-
+    IF v_codigo = 0 THEN
+        INSERT INTO Empleados (
+            codigo,
+            DNI,
+            nombre,
+            direccion,
+            fechaDeComienzo,
+            salario,
+            trabajaPorLaSucursal,
+            codigo_sucursal
+        ) VALUES (
+            p_codigo,
+            p_dni,
+            p_nombre,
+            p_direccion,
+            p_fechaDeComienzo,
+            p_salario,
+            1,
+            p_codigo_sucursal
+        );
+        DBMS_OUTPUT.PUT_LINE('Empleado creado exitosamente.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Ya existe un Empleado con este codigo.');
+    END IF;
+    
     COMMIT;
-
+    
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
@@ -103,22 +111,30 @@ CREATE OR REPLACE PROCEDURE nuevaSucursal(
     p_comunidadAutonoma VARCHAR2,
     p_director NUMBER DEFAULT NULL
 ) IS
+    v_codigo NUMBER(10);
 BEGIN
-    INSERT INTO Sucursales (
-        codigo,
-        nombre,
-        ciudad,
-        comunidadAutonoma,
-        director
-    ) VALUES (
-        p_codigo,
-        p_nombre,
-        p_ciudad,
-        p_comunidadAutonoma,
-        p_director
-    );
-    
-    DBMS_OUTPUT.PUT_LINE('Sucursal creada exitosamente.');
+    SELECT COUNT(*) INTO v_codigo
+    FROM Sucursales
+    WHERE codigo = p_codigo;
+
+    IF v_codigo = 0 THEN
+        INSERT INTO Sucursales (
+            codigo,
+            nombre,
+            ciudad,
+            comunidadAutonoma,
+            director
+        ) VALUES (
+            p_codigo,
+            p_nombre,
+            p_ciudad,
+            p_comunidadAutonoma,
+            p_director
+        );
+        DBMS_OUTPUT.PUT_LINE('Sucursal creada exitosamente.');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Ya existe una Sucursal con este codigo.');
+    END IF;
     
     COMMIT;
 EXCEPTION
@@ -161,23 +177,31 @@ CREATE OR REPLACE PROCEDURE nuevoCliente(
     p_direccion VARCHAR2,
     p_comunidadAutonoma VARCHAR2
 ) IS
+    v_codigo NUMBER(10);
 BEGIN
-    INSERT INTO Clientes(
-        codigo,
-        DNI,
-        tipo,
-        nombre,
-        direccion,
-        comunidadAutonoma)
-    VALUES(
-        p_codigo,
-        p_DNI,
-        p_tipo,
-        p_nombre,
-        p_direccion,
-        p_comunidadAutonoma);
-    
-    DBMS_OUTPUT.PUT_LINE('Cliente creada');
+    SELECT COUNT(*) INTO v_codigo
+    FROM Clientes
+    WHERE codigo = p_codigo;
+
+    IF v_codigo = 0 THEN
+        INSERT INTO Clientes(
+            codigo,
+            DNI,
+            tipo,
+            nombre,
+            direccion,
+            comunidadAutonoma)
+        VALUES(
+            p_codigo,
+            p_DNI,
+            p_tipo,
+            p_nombre,
+            p_direccion,
+            p_comunidadAutonoma);
+        DBMS_OUTPUT.PUT_LINE('Cliente creada');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('Ya existe un cliente con este codigo.');
+    END IF;
     
     COMMIT;
 EXCEPTION
@@ -208,7 +232,7 @@ BEGIN
         p_codigo_sucursal
     );
     
-    DBMS_OUTPUT.PUT_LINE('Suministro creado');
+    DBMS_OUTPUT.PUT_LINE('Suministro creado o actualizado');
     
     COMMIT;
 EXCEPTION
